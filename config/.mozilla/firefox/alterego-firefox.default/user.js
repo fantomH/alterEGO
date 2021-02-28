7551858262899b5b954f28670f86240d2fe005a4
Pascal Malouin <pascal.malouin@gmail.com>
2021-02-28 16:59:25 -0500

    UPDATE alterego-firefox.default

---
+++ config/.mozilla/firefox/alterego-firefox.default/user.js
@@ -0,0 +1,23 @@
+/*-{ ZmFudG9tSA==: "Open the vault of knowledge" }-----------------------------
+#
+# .xinitrc:
+#   created:       '2021-02-23 02:54:43 UTC'
+#   updated:       '2021-02-23 02:54:56 UTC'
+#   description:   'Starts Window Manager with startx.' 
+#------------------------------{ alterEGO Linux }----------------------------*/
+
+user_pref("browser.startup.page", 1);
+user_pref("browser.startup.homepage", "https://fantomh.xyz");
+
+/*-[ CLEAR BROWSING ]                                                        */
+user_pref("privacy.sanitize.sanitizeOnShutdown", true);
+user_pref("privacy.ClearOnShutdown.cookies", true);
+user_pref("privacy.ClearOnShutdown.cache", true);
+user_pref("privacy.ClearOnShutdown.downloads", true);
+user_pref("privacy.ClearOnShutdown.history", true);
+user_pref("privacy.ClearOnShutdown.formData", true);
+user_pref("privacy.ClearOnShutdown.offlineApps", true);
+user_pref("privacy.ClearOnShutdown.sessions", true);
+user_pref("privacy.ClearOnShutdown.siteSettings", true);
+
+/*-{ file:fin }--------------------------------------------------------------*/

77f8e124d11d240633fc2a029b7b8654b28f9fde
Pascal Malouin <pascal.malouin@gmail.com>
2021-03-04 17:33:47 -0500

    UPDATE ffox user.js to remove telemetry

--- config/.mozilla/firefox/alterego-firefox.default/user.js
+++ config/.mozilla/firefox/alterego-firefox.default/user.js
@@ -1,9 +1,9 @@
 /*-{ ZmFudG9tSA==: "Open the vault of knowledge" }-----------------------------
 #
-# .xinitrc:
+# user.js:
 #   created:       '2021-02-23 02:54:43 UTC'
-#   updated:       '2021-02-23 02:54:56 UTC'
-#   description:   'Starts Window Manager with startx.' 
+#   updated:       '2021-03-04 22:24:16 UTC'
+#   description:   'Firefox configuration.' 
 #
 #------------------------------{ alterEGO Linux }----------------------------*/
 
@@ -22,6 +22,24 @@ user_pref("privacy.ClearOnShutdown.sessions", true);
 user_pref("privacy.ClearOnShutdown.siteSettings", true);
 
 /*-[ TELEMETRY ]                                                             */
+user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false);
+user_pref("browser.newtabpage.activity-stream.telemetry", false);
+user_pref("browser.ping-centre.telemetry", false);
+user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("datareporting.policy.dataSubmissionEnabled", false);
+user_pref("toolkit.telemetry.archive.enabled", false);
+user_pref("toolkit.telemetry.bhrPing.enabled", false);
+user_pref("toolkit.telemetry.firstShutdownPing.enabled", false);
+user_pref("toolkit.telemetry.newProfilePing.enabled", false);
+user_pref("toolkit.telemetry.pioneer-new-studies-available", false);
+user_pref("toolkit.telemetry.reportingpolicy.firstRun", false);
+user_pref("toolkit.telemetry.shutdownPingSender.enabled", false);
+user_pref("toolkit.telemetry.unified", false);
+user_pref("toolkit.telemetry.updatePing.enabled", false);
+
+/*-[ OTHER CONFIG ]                                                           */
+/* --- Fix mouse strange behavior with context menu                           */
+user_pref("ui.context_menus.after_mouseup", true);
 
 
 /*-{ file:fin }--------------------------------------------------------------*/

8011af83805e1ee1a87e28efbf5f7f4e61b209bc
Pascal Malouin <pascal.malouin@gmail.com>
2021-03-04 16:54:45 -0500

    UPDATE firefox config

--- config/.mozilla/firefox/alterego-firefox.default/user.js
+++ config/.mozilla/firefox/alterego-firefox.default/user.js
@@ -4,6 +4,7 @@
 #   created:       '2021-02-23 02:54:43 UTC'
 #   updated:       '2021-02-23 02:54:56 UTC'
 #   description:   'Starts Window Manager with startx.' 
+#
 #------------------------------{ alterEGO Linux }----------------------------*/
 
 user_pref("browser.startup.page", 1);
@@ -19,5 +20,8 @@ user_pref("privacy.ClearOnShutdown.formData", true);
 user_pref("privacy.ClearOnShutdown.offlineApps", true);
 user_pref("privacy.ClearOnShutdown.sessions", true);
 user_pref("privacy.ClearOnShutdown.siteSettings", true);
+
+/*-[ TELEMETRY ]                                                             */
+
 
 /*-{ file:fin }--------------------------------------------------------------*/

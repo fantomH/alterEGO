22e16b9ac4bcdb976468ad96accab7d102c475bc
Pascal Malouin <pascal.malouin@gmail.com>
2021-03-04 22:03:41 -0500

    UPDATE _update.sh

--- .tools/_update.sh
+++ .tools/_update.sh
@@ -11,15 +11,23 @@
 
 repo="/usr/local/alterEGO"
 
-repo_firefox="${repo}/config/.mozilla/firefox/alterego-firefox.default"
-local_firefox="${HOME}/.mozilla/firefox/alterego-firefox.default"
+repo_firefox="${repo}/config/.mozilla/firefox/"
+local_firefox="${HOME}/.mozilla/firefox/"
+profile_firefox='alterego-firefox.default'
 
 if [[ ${1} == '--repo' ]]; then
     if [[ ${2} == 'firefox' ]]; then
         printf '%s\n' ":: Will update ${local_firefox} => ${repo_firefox}"
+    fi
 elif [[ ${1} == '--local' ]]; then
     if [[ ${2} == 'firefox' ]]; then
-        printf '%s\n' ":: Will update ${repo_firefox} => ${local_firefox}"
+        printf '%s\n' ":: Will update ${repo_firefox}${profile_firefox} => ${local_firefox}${profile_firefox}"
+	rm -rf ${local_firefox}${profile_firefox}
+	sudo cp -r ${repo_firefox}${profile_firefox} ${local_firefox}
+	sudo chown --recursive ${USER} ${local_firefox}${profile_firefox}
+	
+        printf '%s\n' "-> Done!.."
+    fi
 fi
 
 #--{ file:fin }----------------------------------------------------------------

65e3fb589bb39a7106cda888ef43bb900c379fb8
Pascal Malouin <pascal.malouin@gmail.com>
2021-07-16 22:59:25 -0400

    ...

---
+++ config/.config/egomenu/egoconfig.py
@@ -0,0 +1,15 @@
+from collections import namedtuple
+
+MenuOption = namedtuple('MenuOption', ['name', 'run', 'cmd', 'session', 'description', 'is_floating'])
+
+#### Execute options:
+#... 
+
+additional_options = [
+    MenuOption('all apps', 'launch', 'dmenu_run -nb \'#000\' -l 20 | xargs', 'ALLAPPS', 'Use dmenu_run to see all apps.', False),
+    MenuOption('brave', 'launch', 'brave', 'BRAVE', 'Web browser.', False),
+    MenuOption('dicom', 'terminal', 'bash ${HOME}/fH/scripts/dicomTAG.sh', 'DICOM', 'Browse DICOM tags.', True),
+    MenuOption('emojis', 'stay_in_terminal', r"""bash ${HOME}/fH/scripts/emojis.sh && exit""", 'EMOJIS', 'Select emojis.', True),
+    MenuOption('pamac', 'launch', 'pamac-manager', 'PAMAC', 'Package Manager based on libalpm with AUR and Appstream support.', False),
+    MenuOption('Work RDP', 'launch', 'remmina --connect=/home/ghost/.local/share/remmina/group_rdp_work_ws-18-02-mtl-radiologymontreal-com.remmina', 'WILLWORK', 'Connect to work. mstsc', False),
+    ]

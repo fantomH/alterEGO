56783eb40fc7d603fb1c7bff3999b2491db96710
Pascal Malouin <pascal.malouin@gmail.com>
2021-10-14 05:54:44 -0400

    ...

--- bin/mounter.py
+++ bin/mounter.py
@@ -2,15 +2,16 @@
 
 ## { alterEGO Linux: "Open the vault of knowledge" } ----------------------- ##
 ##                                                                           ##
-## mounter.py                                                                ##
+## /usr/local/bin/mounter.py                                                 ##
 ##   created       : 2021-10-05 11:36:11 UTC                                 ##
-##   updated       : 2021-10-07 11:20:12 UTC                                 ##
+##   updated       : 2021-10-13 09:48:13 UTC                                 ##
 ##   description   : mount and unmount utility.                              ##
 ## _________________________________________________________________________ ##
 
 from collections import namedtuple
 import json
 import os
+import random
 import shlex
 import subprocess
 
@@ -36,29 +37,74 @@ def devices():
     DEVICE = namedtuple('DEVICE', ['label', 'path', 'size', 'fstype', 'mountpoint'])
     DEVICES_LIST = []
 
+    ## ( GENERAL DEVICES )
+
     lsblk_devices = subprocess.run(shlex.split(f"lsblk -Jp -o NAME,SIZE,FSTYPE,MOUNTPOINT,LABEL"), stdout=subprocess.PIPE)
 
     data = json.loads(lsblk_devices.stdout.decode('UTF-8'))
     for d in data['blockdevices']:
         if d.get('children'):
             d_dict = d.get('children')[0]
+            ## >_ Catching devices without labels.
+            if d_dict.get('label') == None:
+                _label = f"DEVICE{str(random.randint(0, 99999)).zfill(5)}"
+            else:
+                _label = d_dict.get('label') 
             DEVICES_LIST.append(DEVICE(
-                                    d_dict.get('label'),
+                                    _label,
                                     d_dict.get('name'),
                                     d_dict.get('size'),
                                     d_dict.get('fstype'),
                                     d_dict.get('mountpoint')
                                         ))
         else:
+            ## >_ Catching devices without labels.
+            if d_dict.get('label') == None:
+                _label = f"DEVICE{str(random.randint(0, 99999)).zfill(5)}"
+            else:
+                _label = d_dict.get('label') 
             DEVICES_LIST.append(DEVICE(
-                                    d.get('label'),
+                                    _label,
                                     d.get('name'),
                                     d.get('size'),
                                     d.get('fstype'),
                                     d.get('mountpoint')
                                         ))
 
-    EXCLUDED_DEVICES = ['/dev/sda1', '/dev/sr0']
+    ## ( ANDROID DEVICES )
+
+    android_devices = []
+    ## >_ Find the special files in /dev (libmtp-*)
+    for f in [ x for x in os.listdir('/dev/') if x.startswith('libmtp')]:
+        _path = f"/dev/{f}"
+
+        ## >_ Find the symlink of /dev/libmtp*
+        _symlink = os.readlink(_path).split('/')
+
+        ## >_ Build lsusb arguments like: `lsusb -s 003:011 -v` and run.
+        _lsusb = subprocess.run(shlex.split(f"lsusb -s {_symlink[-2]}:{_symlink[-1]} -v"), stdout=subprocess.PIPE).stdout.decode('UTF-8').split('\n')
+
+        ## >_ Build "label name"
+        ## .. grep iManufacturer           1 HUAWEI
+        ## ..      iProduct                2 MAR-LX3A
+        for x in _lsusb:
+            if 'iManufacturer' in x:
+                _iManufacturer = x.split()[-1]
+            if 'iProduct' in x:
+                _iProduct = x.split()[-1]
+        _label = f"{_iManufacturer}-{_iProduct}"
+
+        DEVICES_LIST.append(DEVICE(
+                                _label,
+                                _path,
+                                'UNK_SIZE',
+                                "Android",
+                                None
+                                    ))
+
+    ## >_ TODO Not really working...
+    ## .. Either sr0 or sr1 shows up.
+    EXCLUDED_DEVICES = ['/dev/sda1', '/dev/sr0', '/dev/sr1']
     for device in DEVICES_LIST:
         if device.path in EXCLUDED_DEVICES:
             DEVICES_LIST.remove(device)
@@ -68,9 +114,23 @@ def devices():
 def to_mount(DEVICE_PROPERTIES):
     print(f"I will mount {DEVICE_PROPERTIES.label}")
 
+    MOUNTPOINT = f"/run/media/ghost/{DEVICE_PROPERTIES.label}"
+    if not os.path.isdir(MOUNTPOINT):
+        os.makedirs(MOUNTPOINT)
+
+    if DEVICE_PROPERTIES.fstype == "ntfs":
+        subprocess.run(shlex.split(f"sudo ntfs-3g {DEVICE_PROPERTIES.path} \"{MOUNTPOINT}\" -o uid=1000,gid=1000,utf8,dmask=027,fmask=137"))
+    elif DEVICE_PROPERTIES.fstype == "Android":
+        subprocess.run(shlex.split(f"simple-mtpfs {DEVICE_PROPERTIES.path} \"{MOUNTPOINT}\" -o allow_other"))
+    else:
+        subprocess.run(shlex.split(f"sudo mount -t auto {DEVICE_PROPERTIES.path} \"{MOUNTPOINT}\" -o uid=1000,gid=1000,utf8,dmask=027,fmask=137"))
+
 def main():
 
     DEVICES = devices()
+
+    for d in DEVICES:
+        print(d)
 
     DEVICES_SELECTION = ''.join([f"{d.label:<15}[ {d.size:<10}{d.path:<20}{d.fstype:<10}{d.mountpoint} ]\n" for d in DEVICES]).encode('UTF-8')
 
@@ -85,5 +145,9 @@ def main():
     
 if __name__ == '__main__':
     main()
+
+## [ TODO ] ---------------------------------------------------------------- ##
+
+    ## - Unmount everything if mounted and delete directories.
 
 ## FIN _____________________________________________________________ ¯\_(ツ)_/¯
